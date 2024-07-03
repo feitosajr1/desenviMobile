fb0bed7ab1faa5401f66efba227c242980168d0c
Feitosa Junior <juniorfeito2016@gmail.com>
2024-07-03 10:52:44 -0300

    Revisando

--- a.01/app-primeira-aula/src/app/app.module.ts
+++ a.01/app-primeira-aula/src/app/app.module.ts
@@ -9,10 +9,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header-components';
 import { RodapeComponent } from './components/rodape-components';
 
-@NgModule({
-  declarations: [AppComponent, HeaderComponent, RodapeComponent],
+@NgModule({ //Após criar o componente (header-components), precisamos instanciar o componente nos modulos. Instanciamos a classe exportada no Ts do componente que criamos (export class HeaderComponent)
+  declarations: [AppComponent, HeaderComponent, RodapeComponent], //Agora esse componente está disponivél no modulo e em qualquer página agora, podemos usar o HeaderComponents
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
--- a.01/app-primeira-aula/src/app/components/header-components.ts
+++ a.01/app-primeira-aula/src/app/components/header-components.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //import é feito para que tenha valor o @component
 
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.components.html',
+@Component({ //Chamado de decorator, serve para ensinar ao framework o que é essa classe, um componente. (O componente é configurado com objetos)
+  selector: 'app-header', //O seletor indica qual que é a tag que vai ser utilizada para instancionar o componente nos locais que utilizam ele.
+  templateUrl: './header.components.html', //Esse objeto é a página html que foi criada para o componente "header-components.html". O html também pode ser escrito aqui usando o objeto "template:"
+  // styleUrl: "" também pode ser usado para o estilo do componente,
 })
 export class HeaderComponent {}
+
+// Continua em App.module.ts
